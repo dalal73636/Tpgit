08f142dd400c260eb4bba9a39b116a5be3d31d1f
ETTIFOURI El Hassane <h.ettifouri@gmail.com>
2024-01-03 23:16:18 +0100

    Initial commit 	new file:   App.java 	new file:   Calculatrice.java

---
+++ App.java
@@ -0,0 +1,16 @@
+// App.java
+
+public class App {
+    public static void main(String[] args) {
+        // ... existing code ...
+
+        // Example calls to the multiplication function
+        Calculatrice calculator = new Calculatrice();
+        int result1 = calculator.multiply(5, 3);
+        int result2 = calculator.multiply(2, 7);
+
+        System.out.println("Multiplication Results:");
+        System.out.println("Result 1: " + result1);
+        System.out.println("Result 2: " + result2);
+    }
+}
---
+++ Calculatrice.java
@@ -0,0 +1,10 @@
+// Calculatrice.java
+
+public class Calculatrice {
+   
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Other methods for the calculator class
+}

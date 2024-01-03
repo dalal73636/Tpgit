fa393f5ad81ffcaa94d915ea430f36d6d3f09792
ETTIFOURI El Hassane <h.ettifouri@gmail.com>
2024-01-03 23:39:43 +0100

    modified:   Calculatrice.java

--- Calculatrice.java
+++ Calculatrice.java
@@ -3,8 +3,8 @@
 public class Calculatrice {
    
    
-    public int add(int a, int b) {
-        return a + b;
+    public int multiplication(int a, int b) {
+        return a * b;
     }
 
     // Other methods for the calculator class

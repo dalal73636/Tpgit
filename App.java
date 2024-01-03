688ce3bb97ac84b188eb4a774419e709a24899a3
ETTIFOURI El Hassane <h.ettifouri@gmail.com>
2024-01-03 23:37:44 +0100

    modified:   App.java 	modified:   Calculatrice.java

--- App.java
+++ App.java
@@ -7,8 +7,8 @@ public class App {
 
         // Example calls to the multiplication function
         Calculatrice calculator = new Calculatrice();
-        int result1 = calculator.multiply(5, 3);
-        int result2 = calculator.multiply(2, 7);
+        int result1 = calculator.multiplication(5, 3);
+        int result2 = calculator.multiplication(2, 7);
 
         System.out.println("Multiplication Results:");
         System.out.println("Result 1: " + result1);

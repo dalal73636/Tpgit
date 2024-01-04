632a0d73f44814518dcd14b23e9d7dd8c363fed2
malki1997-dev <77008792+malki1997-dev@users.noreply.github.com>
2024-01-04 17:38:39 +0100

    Update App.java

--- App.java
+++ App.java
@@ -10,6 +10,7 @@ public class App {
         int result1 = calculator.multiplication(5, 3);
         int result2 = calculator.add(2, 7);
 
+        
         System.out.println("*** Results:");
         System.out.println("Result 1: " + result1);
         System.out.println("Result 2: " + result2);

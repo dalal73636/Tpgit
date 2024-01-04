ad53b0bc1dccdbf246e0ca2dc3c27894f86db1a6
malki1997-dev <77008792+malki1997-dev@users.noreply.github.com>
2024-01-04 16:52:16 +0100

    Update App.java

--- App.java
+++ App.java
@@ -4,13 +4,13 @@ public class App {
     
     public static void main(String[] args) {
         // ... existing code ...
-
+        
         // Example calls to the multiplication function
         Calculatrice calculator = new Calculatrice();
-        int result1 = calculator.multiply(5, 3);
-        int result2 = calculator.multiply(2, 7);
+        int result1 = calculator.multiplication(5, 3);
+        int result2 = calculator.add(2, 7);
 
-        System.out.println("Multiplication Results:");
+        System.out.println("*** Results:");
         System.out.println("Result 1: " + result1);
         System.out.println("Result 2: " + result2);
     }
